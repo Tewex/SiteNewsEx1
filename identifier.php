d0c287baa5df9c7a3226ad13a478f10700cbb0e2
Tewex <35960596+Tewex@users.noreply.github.com>
2018-08-30 11:07:02 +0200

    Inscription
    Inscription

---
+++ identifier.php
@@ -0,0 +1,30 @@
+<?php
+// put your code here
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="style/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <fieldset>
+            <legend>Connection</legend>
+            <form action="#" method="POST" >
+                <label>Identifiant:</label>
+                <input type="text" name="tbxIdentifiant">
+                <label>Mot de passe:</label>
+                <input type="text" name="tbxMdp">
+            </form>
+            <button  name="btnLogin">Valider</button>
+        </fieldset>
+        
+        <label><a href="./inscription.php" >Pas encore inscrit?</a></label>
+    </body>
+</html>
